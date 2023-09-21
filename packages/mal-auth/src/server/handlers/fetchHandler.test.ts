62df181531c703395269466d743b00ce01d7a1a1
Freddy L <neo-ciber_94@hotmail.com>
2023-09-20 22:50:35 -0400

    minor changes

--- packages/mal-auth/src/server/handlers/fetchHandler.test.ts
+++ packages/mal-auth/src/server/handlers/fetchHandler.test.ts
@@ -1,13 +1,12 @@
 import { describe, expect, test, vi } from 'vitest';
 import { handleAuthFetchRequest } from './fetchHandler';
-import { type Cookies } from '../common/types';
+import { type Cookies } from '../../common/types';
 
 function getFakeCookies(): Cookies {
     return {
         set: () => { },
         get: () => null,
         delete: () => null,
-        has: () => false
     }
 }
 
