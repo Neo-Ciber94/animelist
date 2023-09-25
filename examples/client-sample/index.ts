1303ae7b665633a17db995f2c6573d58fbba0f1b
Freddy L <neo-ciber_94@hotmail.com>
2023-09-24 22:01:29 -0400

    Updated client sample

--- examples/client-sample/index.ts
+++ examples/client-sample/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import { MALClient } from "@animelist/client";
+import type { AnimeSeason } from "@animelist/core"
 
 dotenv.config();
 
@@ -7,8 +8,14 @@ const client = new MALClient({
     clientId: process.env.MY_ANIME_LIST_CLIENT_ID,
 });
 
+function getCurrentSeason(): AnimeSeason {
+    const currentMonth = new Date().getMonth();
+    const seasons = ["winter", "spring", "summer", "fall"] as const;
+    return seasons[Math.floor(currentMonth / 3)];
+}
+
 const animeList = await client.getSeasonalAnime({
-    season: 'summer',
+    season: getCurrentSeason(),
     year: new Date().getFullYear(),
     limit: 10
 });
