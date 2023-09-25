1f9a7d5b161d0dd496ecb629fa8b7324898c488a
Freddy L <neo-ciber_94@hotmail.com>
2023-09-24 21:10:48 -0400

    Updated entrypoints

---
+++ scripts/cleanUpEntrypoints.ts
@@ -0,0 +1,21 @@
+import * as glob from "glob";
+import fse from 'fs-extra';
+import path from "path";
+import { ENTRYPOINT_MARKER } from "./entrypoints";
+
+function main() {
+    console.log("Removing entry points...")
+    const entryPointMarkers = glob.sync(`**/**/${ENTRYPOINT_MARKER}`);
+
+    for (const entryPointFile of entryPointMarkers) {
+        if (!fse.exists(entryPointFile)) {
+            continue;
+        }
+
+        const dir = path.dirname(entryPointFile);
+        fse.removeSync(dir);
+        console.log(`Removed: ${dir}`)
+    }
+}
+
+main();
--- scripts/entrypoints.ts
+++ scripts/entrypoints.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import fse from 'fs-extra';
 import * as glob from 'glob';
 
+export const ENTRYPOINT_MARKER = ".entrypoint";
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function writeFileSync(filePath: string, contents: string) {
     const dir = path.dirname(filePath);
@@ -11,13 +13,17 @@ function writeFileSync(filePath: string, contents: string) {
 }
 
 export function generateEntrypoints(packageDir: string) {
-    const srcDir = path.resolve(packageDir, 'src');
+    const srcDir = path.resolve(packageDir, 'dist');
 
     if (!fse.existsSync(srcDir)) {
         throw new Error(`'src' directory does not exist on ${srcDir}`);
     }
 
-    const indexFiles = glob.sync(`${srcDir}/**/**/index.ts`);
+    cleanUpEntrypoints(packageDir);
+    const indexFiles = [
+        ...glob.sync(`${srcDir}/**/index.ts`),
+        ...glob.sync(`${srcDir}/**/index.tsx`)
+    ];
 
     for (const indexFile of indexFiles) {
         const relativePath = path.dirname(path.relative(srcDir, indexFile));
@@ -33,14 +39,29 @@ export function generateEntrypoints(packageDir: string) {
         const moduleEntryPointFilePath = path.join(packageDir, relativePath, "index.js");
         const typeEntryPointFilePath = path.join(packageDir, relativePath, "index.d.ts");
         const gitignoreFilePath = path.join(packageDir, relativePath, ".gitignore");
+        const entryPointMarkerFilePath = path.join(packageDir, relativePath, ".entrypoint");
 
         writeFileSync(moduleEntryPointFilePath, moduleExport);
         writeFileSync(typeEntryPointFilePath, typesExport);
         writeFileSync(gitignoreFilePath, "*");
+        writeFileSync(entryPointMarkerFilePath, new Date().toUTCString());
 
         console.log({
             moduleEntryPointFilePath,
             typeEntryPointFilePath
         })
+    }
+}
+
+export function cleanUpEntrypoints(packageDir: string) {
+    const entryPointMarkers = glob.sync(`${packageDir}/**/${ENTRYPOINT_MARKER}`);
+
+    for (const entryPointFile of entryPointMarkers) {
+        if (!fse.existsSync(entryPointFile)) {
+            continue;
+        }
+
+        const dir = path.dirname(entryPointFile);
+        fse.removeSync(dir);
     }
 }
