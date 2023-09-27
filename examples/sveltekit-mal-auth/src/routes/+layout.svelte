0eea1734e19e125b22ab21de6afbe99016627b6e
Freddy L <neo-ciber_94@hotmail.com>
2023-09-26 20:20:36 -0400

    minor changes

--- examples/sveltekit-mal-auth/src/routes/+layout.svelte
+++ examples/sveltekit-mal-auth/src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { session } from '@animelist/auth-sveltekit/client';
-	import type { LayoutServerData } from './$types';
 
-	export let data: LayoutServerData;
+	// Uncomment this and invoke `session.initialize(data.session)` to load the user in SSR
+	// import type { LayoutServerData } from './$types';
+	// export let data: LayoutServerData;
 
-	session.initialize(data.session);
+	// This initializes the store and fetches the user
+	session.initialize().catch(console.error);
 </script>
 
 <slot />
