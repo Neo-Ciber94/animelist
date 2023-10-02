a4f522842bab890456c57114e30d9826c22f4824
Freddy L <neo-ciber_94@hotmail.com>
2023-10-01 21:02:09 -0400

    Updated NextJS example

--- examples/next-mal-auth/src/app/page.tsx
+++ examples/next-mal-auth/src/app/page.tsx
@@ -1,10 +1,5 @@
-import { type Metadata } from "next";
 import AnimeListSuggestion from "./AnimeListSuggestion";
 import Auth from "./Auth";
-
-export const metadata: Metadata = {
-  title: "MyAnimeList - NextJS",
-};
 
 export default function HomePage() {
   return (
--- examples/next-mal-auth/src/app/providers.tsx
+++ examples/next-mal-auth/src/app/providers.tsx
@@ -4,7 +4,7 @@ import { SessionProvider, type Session } from "@animelist/auth-next/client";
 
 type MyAnimeListAuthProviderProps = {
   children: React.ReactNode;
-  session?: Session;
+  session?: Session | null;
 };
 
 // <SessionProvider> cannot be used as a server component, so we wrap it.
