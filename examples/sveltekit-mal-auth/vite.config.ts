41275a916cd157df39bfc9cbe208b7b9ecb0ac6e
Freddy L <neo-ciber_94@hotmail.com>
2023-10-05 18:52:26 -0400

    Updated sveltekit example

--- examples/sveltekit-mal-auth/vite.config.ts
+++ examples/sveltekit-mal-auth/vite.config.ts
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -15,12 +16,16 @@ const defineProcessEnv = () => {
 };
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), nodePolyfills()],
 	server: {
 		port: 3000
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
-	define: defineProcessEnv(),
+	//define: defineProcessEnv()
+	define: {
+		'process.versions': JSON.stringify(process.versions),
+		...defineProcessEnv()
+	}
 });
